348622db231eb4300cc4c4108fab5e3c76ca3077
Anna Desnenko <97408707+VioleetSky@users.noreply.github.com>
2026-08-17 19:38:28 +0300

    Add homework 15

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+function setInformation(req, res, next) {
+console.log(`${req.method.toUpperCase()} - ${req.url}, ${new Date().toLocaleString('uk-UA')} `);
+next();
+}
+app.use(setInformation);
+
+app.post("/feedback", (req, res) => {
+    if(!req.body.name){
+        res.status(400).send("Поле name є обов'язковим")
+    }
+    else {
+        res.send(`Дякуємо, ${req.body.name}! Ваш відгук отримано.\n`)
+    }
+})
+
+app.get('/crash', (req, res, next) => {
+    throw new Error('Тестова помилка');
+});
+
+app.get('/', (req, res) => {
+    res.send("Main page");
+})
+app.get('/about', (req, res) => {
+    res.send("About page");
+})
+
+app.get('/time', (req, res) => {
+    res.send('Поточний час: ' + new Date().toLocaleString('uk-UA'));
+})
+
+app.get('/error', (req, res) => {
+    res.status(500).send('Server error');
+})
+
+app.get("/user/:id", (req, res) => {
+    res.send("User ID: " + req.params.id);
+})
+
+app.get("/search", (req, res) => {
+    const query=req.query.q
+    if(!query){
+        res.status(400).send('No query found.');
+    }
+    res.send(`Search page ${query}`);
+})
+
+app.get("/user/:id/orders", (req, res) => {
+    res.send("User ID: " + req.params.id+ " with status: "+ req.query.status);
+})
+app.use((req, res) => {
+    res.status(404).send('Page Not Found');
+})
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(' Щось пішло не так!');
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Server started on port 3000');
+})
